8e9ed93459f8dc546dd93bce7a384b149a9ceaeb
CARLA LOPEZ <carlalolajeanne@gmail.com>
2024-10-25 14:58:55 +0200

    :bug: correction erreurs console slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -22,19 +22,20 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.id}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+          <div key={event.title}>
+            <div>
+              <div
+                className={`SlideCard SlideCard--${
+                  index === idx ? "display" : "hide"
+                }`}
+              >
+              <img src={event.cover} alt="forum" />
+              <div className="SlideCard__descriptionContainer">
+                <div className="SlideCard__description">
+                  <h3>{event.title}</h3>
+                  <p>{event.description}</p>
+                  <div>{getMonth(new Date(event.date))}</div>
+                </div>
               </div>
             </div>
           </div>
@@ -46,11 +47,12 @@ const Slider = () => {
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
